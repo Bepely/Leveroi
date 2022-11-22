526cf0b98e8d113f42f87f91e862ee86e8c4415c
Bepely <ngogin7@gmail.com>
2022-11-22 22:14:11 +0300

    fixin2

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b5c677c32b99c548"; }
+/******/ 		__webpack_require__.h = function() { return "09c8cbe5f071838a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.5a78f9d7ffe43745.hot-update.js
+++ .next/static/webpack/webpack.5a78f9d7ffe43745.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "b5c677c32b99c548"; }
+/******/ 	__webpack_require__.h = function() { return "6303219f5a698d56"; }
 /******/ }();
 /******/ 
 /******/ }
