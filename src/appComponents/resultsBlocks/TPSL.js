9c86f2cc3e591f47a572c3a41df0c3de4fa1d6d7
Bepely <ngogin7@gmail.com>
2022-12-13 23:56:45 +0300

    =///////
    DOESNT WORKING LIMITS
    BULSHIT

--- src/appComponents/resultsBlocks/TPSL.js
+++ src/appComponents/resultsBlocks/TPSL.js
@@ -6,105 +6,107 @@ const TPSL = ({open, close}) => {
 
 //init value of TP/SL is a false. 
 //if user set up a value for TP/LS it is no longer false and have a price.
-const [TP, setTP] = useState({init: false, price: 0, long: close.long})
-const [SL, setSL] = useState({init: false, price: 0, long: close.long})
 
-const userTPset = () => {setTP(TP => ({...TP, init:true, price:close.price, long: close.long }))}
-const userTPremove = () => {setTP(TP => ({...TP, init: false, price: 0, long: close.long}))}
+const [limits, setLimits] = useState([-1, -1])
 
-const userSLset = () => setSL(SL => ({...SL, init:true, price:close.price, long: close.long }))
-const userSLremove = () => {setSL(SL => ({...SL, init: false, price: 0, long: close.long}))}
+//13.12.22
+//23:53 Bepely
+//Нихуя не работает, я в ахуе. 
+//туплю на ровном месте,, не выкупаю нихуя. 
+//надо сделать тут чтоб было максимум 2 лимита открытых, но если их нет ни 1 - показывай большую кнопку создать
+//если есть 1 лимит из 2 - показывай выставленный лимит и кнопку добавления лимита
+//если создано 2 лимита - показывай 2 лимита и не показывай кнопкую 
+
+//жи есть все просто. я в ахуе туплю и жопу рвет
+//все, в призду, завтра доделаю
+
+
+const setNewLimit = (item) => {
+  console.log(item);
+  _limits[item.id] = item;
+
+   
+   
+  }
+
+  const removeLimit = (number) => {
+  
+    _limits = limits
+   
+    _limits.splice(number, 1)
+    _limits = _limits.concat({init: false, price: 0, long:close.long, id:id})
+    
+    
+    setLimits(_limits)
+
+
+  }
+
+
 
 useEffect(() => {
-  let _TP = TP 
-  let _SL = SL 
+  _limits = limits
+ 
+  _limits.forEach((element, index) => {
+    _limits[index].long = close.long;
+  });
 
- setTP({..._SL, long: close.long})
- setSL({..._TP, long: close.long})
-}, [close.long])
+  setLimits(limits => _limits)
+  console.log(limits);
+
+}, [close])
 
 
-  return (
-    <div className="layerBase multiHor" id='layerBaseTPSL'>
-      {TP.init ? 
-      <div className="layerBase miltiVer" id='TPSLbase'>
-        
-        <div className='layerFloor blockCard miltiVer'>
-            <div className="pnlResults" id='pnlTPSL'> 
-                      <div className='TPSLcontainer' id='textContent'>
-                        <h4>TP Price</h4>
-                        <h5 id='pnlTPSLright'>{TP.price}</h5>
-                      </div>
-                      <div className="pnlContainer " id='textContent'>
-                        <h4>PNL</h4>
-                        <h5>{lcl.margin(open, TP)}</h5>
-                        <h5 id='pnlTPSLright'>{lcl.marginPercent(open, TP)}%</h5>
-                      </div>
-                      <div className="totalContainer" id='textContent'>
-                        <h4>Total</h4>
-                        <h5 id='pnlTPSLright'>{lcl.total(open, TP)}</h5>
-                      </div>
-              </div>
-          </div>
 
-          <div className="layerTable" id="blockButton2">
-            <button onClick={userTPremove}>delete TP</button>
-            <button onClick={userTPset}>set TP</button>
-          </div>
-     
-          
-      </div> 
-      
-      : 
 
-     
-        
-        <button className="blockButton blockCard paddingContent" onClick={userTPset}>
-            Set Take Profit
-        </button>}
-    
+return (
 
-        {SL.init ? 
-      
-            <div className="layerBase miltiVer" id='TPSLbase'>
-              
-              <div className='layerFloor blockCard miltiVer'>
-                  <div className="pnlResults" id='pnlTPSL'> 
-                            <div className='TPSLcontainer' id='textContent'>
-                              <h4>SL Price</h4>
-                              <h5 id='pnlTPSLright'>{SL.price}</h5>
-                            </div>
-                            <div className="pnlContainer " id='textContent'>
-                              <h4>PNL</h4>
-                              <h5>{lcl.margin(open, SL)}</h5>
-                              <h5 id='pnlTPSLright'>{lcl.marginPercent(open, SL)}%</h5>
-                            </div>
-                            <div className="totalContainer" id='textContent'>
-                              <h4>Total</h4>
-                              <h5 id='pnlTPSLright'>{lcl.total(open, SL)}</h5>
-                            </div>
-                    </div>
-                </div>
 
-                <div className="layerTable" id="blockButton2">
-                  <button onClick={userSLremove}>delete SL</button>
-                  <button onClick={userSLset}>set SL</button>
-                </div>
-          
+  <div className="layerBase multiHor" id='layerBaseTPSL'>
+      {limits.forEach(l => (
+        <div key={l.id} className="layerBase miltiVer" id={l.id >= 2 ? 'TPSLbaseNotToShow': 'TPSLbase'}>
+          { l.init ?
                 
-            </div> 
-      
-      : 
+            <>
+                  
+            <div className='layerFloor blockCard miltiVer'>
+                <div className="pnlResults" id='pnlTPSL'> 
 
-     
-        
-        <button className="blockButton blockCard paddingContent" onClick={userSLset}>
-            Set Stop Loss
-        </button>}
+                          <div className='TPSLcontainer' id='textContent'>
+                            <h4>Limit price</h4>
+                            <h5 id='pnlTPSLright'>{l.price}</h5>
+                          </div>
+
+                          <div className="pnlContainer " id='textContent'>
+                            <h4>PNL</h4>
+                            <h5>{lcl.margin(open, l)}</h5>
+                            <h5 id='pnlTPSLright'>{lcl.marginPercent(open, l)}%</h5>
+                          </div>
+
+                          <div className="totalContainer" id='textContent'>
+                            <h4>Total</h4>
+                            <h5 id='pnlTPSLright'>{lcl.total(open, l)}</h5>
+                          </div>
+                  </div>
+            </div>
+            <button className="blockButton paddingContent" onClick={()=>{removeLimit(l.id)}}>delete limit</button>
+
+          </>
 
 
-    </div>
-  )
-}
+            :<></>
+          }
+         
+
+              
+      </div>
+      ))}
+      {!limits[0].init}
+      <button className="blockButton paddingContent" onClick={()=>setLimits({...limits, })}>
+              Set Limit
+      </button>
+  </div>
+
+)}
 
 export default TPSL
